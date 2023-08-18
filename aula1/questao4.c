41a508dce548491dc61342e94bdfcee999404fd4
LUIS EDUARDO MARINHO <107900365+DuduMarinho@users.noreply.github.com>
2023-08-18 15:17:35 -0300

    Atualização da questao 4

--- aula1/questao4.c
+++ aula1/questao4.c
@@ -2,4 +2,25 @@
 fixo e o total de vendas efetuada por ele no mês (em dinheiro). Sabendo
 que este vendedor ganha 15% de comissão sobre suas vendas efetuadas,
 informar o seu nome, o salário fixo e o salário no fim do mês. */
+#include <stdio.h>
 
+int main() 
+{
+    //declaração de variáveis
+    char nome;
+    float salarioFixo, comissao, totalDeGanhos;
+    int totalDeVendasMes;
+    
+    //entrada do usuario 
+    printf("Informe seu nome: ");
+    scanf("%s", &nome);
+    printf("Informe seu salário fixo: ");
+    scanf("%f", &salarioFixo);
+    printf("Quantidade de vendas no mês: ");
+    scanf("%d", &totalDeVendasMes);
+
+    //calculo de comissão
+    comissao = totalDeVendasMes * 0.15;
+    
+
+}
